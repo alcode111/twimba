3351e7aac50480fe9839848eb3eff1015eacb068
Ismail <141173099+alcode111@users.noreply.github.com>
2024-03-02 21:36:34 +0100

    Create a handleReplyClick function to add functionality to display or hide a tweet's reply

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ document.addEventListener('click', function(e) {
         handleLikeClick(e.target.dataset.like)
     } else if (e.target.dataset.retweet) {
         handleRetweetClick(e.target.dataset.retweet)
+    } else if(e.target.dataset.reply) {
+        handleReplyClick(e.target.dataset.reply)
     }
 })
 
@@ -28,6 +30,10 @@ function handleRetweetClick(tweetId) {
     }
     targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted
     render()
+}
+
+function handleReplyClick(replyId) {
+    document.getElementById(`replies-${replyId}`).classList.toggle('hidden')
 }
 
 function getFeedHtml() {
