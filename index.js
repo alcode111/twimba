3cdf4dc5d5209eb1fcff54f2fd6c76526bcb0aea
Ismail <141173099+alcode111@users.noreply.github.com>
2024-03-02 21:54:19 +0100

    Import a function to create uuids

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { tweetsData } from './data.js'
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid'
 
 document.addEventListener('click', function(e) {
     if (e.target.dataset.like) {
@@ -39,7 +40,7 @@ function handleReplyClick(replyId) {
 }
 
 function handleTweetBtnClick() {
-    
+
 }
 
 function getFeedHtml() {
